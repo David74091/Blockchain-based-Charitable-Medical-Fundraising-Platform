161a37217c2225f0db900ea881de27da4aca99cb
David74091 <C109190145@nkust.edu.tw>
2024-02-14 03:47:12 +0800

    change home page style.

--- client/src/pages/client/Home.jsx
+++ client/src/pages/client/Home.jsx
@@ -1,41 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { DisplayCases, PageLoading } from "../../components";
-import {
-  notion,
-  blockChain1,
-  blockChain2,
-  blockChain3,
-  blockChain4,
-  blockChain5,
-  blockChain6,
-  donate1,
-  donate2,
-} from "../../assets";
 
 const Home = (props) => {
-  const images = [
-    blockChain1,
-    blockChain2,
-    blockChain3,
-    blockChain4,
-    blockChain5,
-    blockChain6,
-    donate1,
-    donate2,
-  ];
-  const shuffledImages = images.sort(() => Math.random() - 0.5);
-
-  let {
-    setOnHome,
-    onHome,
-    currentUser,
-    setCurrentUser,
-    caseData,
-    Loading,
-    setInCampaignPage,
-  } = props;
-  const navigate = useNavigate();
+  let { setInCampaignPage } = props;
 
   const [isScrolled, setIsScrolled] = useState(false);
   useEffect(() => {
@@ -62,73 +30,41 @@ const Home = (props) => {
 
   return (
     <div className="relative opacity-0 transition-opacity duration-500 animate-fade-in-forwards">
-      <img
-        className="absolute top-48 left-20 w-[50px] transform -translate-x-1/2 -translate-y-1/2 origin-center rotate-45"
-        src={blockChain1}
-        alt="Example"
-      />
-      <img
-        className="absolute top-[50vh] left-1/4 w-[50px] transform -translate-x-1/2 -translate-y-1/2 origin-bottom -rotate-45"
-        src={blockChain2}
-        alt="Example"
-      />
-      <img
-        className="absolute top-[40vh] right-[10vw] w-[50px] transform translate-x-1/2 -translate-y-1/2 origin-center rotate-45"
-        src={blockChain3}
-        alt="Example"
-      />
-      <img
-        className="absolute top-[60vh] right-[300px] w-[50px] transform translate-x-1/2 -translate-y-1/2 origin-center rotate-12"
-        src={blockChain4}
-        alt="Example"
-      />
-      <img
-        className="absolute bottom-[500px] left-40 w-[50px] transform -translate-x-1/2 translate-y-1/2 origin-center"
-        src={blockChain5}
-        alt="Example"
-      />
-      <img
-        className="absolute bottom-[1200px] left-1/4 w-[50px] transform -translate-x-1/2 translate-y-1/2 origin-center rotate-45"
-        src={blockChain6}
-        alt="Example"
-      />
-      <img
-        className="absolute bottom-50 right-1/4 w-[50px] transform translate-x-1/2 translate-y-1/2 origin-center rotate-45"
-        src={donate1}
-        alt="Example"
-      />
-      <img
-        className="absolute bottom-[500px] right-[50vw] w-[50px] transform -translate-x-1/2 -translate-y-1/2 origin-bottom -rotate-12 "
-        src={donate2}
-        alt="Example"
-      />
       <div className="flex flex-col items-center">
-        <div className="flex flex-col items-center h-1280px w-full mt-40">
-          <div className="h-2/3 flex flex-col items-center">
-            <button className="btn btn-secondary">
-              <img src={notion} />
-            </button>
-            <br />
-            <br />
-            <p className="text-5xl font-bold">現在就在區塊鏈上</p>
-            <br />
-            <br />
-            <p className="text-5xl font-bold">捐款或募款</p>
-            <br />
-            <br />
-
-            <p className="text-[1rem] font-light">享受金流的完全透明</p>
-          </div>
-          <div className="h-1/3 mt-10">
-            <Link
-              to="/register"
-              class="btn btn-secondary text-xl w-[150px] h-[70px]"
-            >
-              立即開始
-            </Link>
-          </div>
+        <div className="flex flex-row justify-center h-1280px w-full mt-40 gap-20">
+          <Link
+            to="CampaignPage"
+            className="bg-accent card w-96 h-96 bg-base-100 shadow-xl p-10"
+          >
+            <div className="w-full h-full flex flex-col justify-center items-center	">
+              <figure>
+                <img
+                  className="object-cover h-full"
+                  src="https://cdn-icons-png.flaticon.com/256/8662/8662585.png"
+                />
+              </figure>
+              <div className="card-body">
+                <h2>瀏覽所有募款</h2>
+              </div>
+            </div>
+          </Link>
+          <Link
+            to="Register"
+            className="bg-secondary card w-96 h-96 bg-base-100 shadow-xl p-10"
+          >
+            <div className="w-full h-full flex flex-col justify-center items-center	">
+              <figure>
+                <img
+                  className="object-cover h-full"
+                  src="https://cdn-icons-png.flaticon.com/256/8662/8662284.png"
+                />
+              </figure>
+              <div className="card-body">
+                <h2>立即註冊</h2>
+              </div>
+            </div>
+          </Link>
         </div>
-        {/* The following code will be animated when scrolled */}
         <div
           className={`divider mt-80 ${
             isScrolled
@@ -166,27 +102,15 @@ const Home = (props) => {
           >
             <li className="step step-secondary">
               <p className="text-xl font-bold ">捐款</p>
-              <p className="font-light text-[0.5rem] mt-2">
-                使用區塊鏈錢包
-                <br />
-                或使用傳統金流
-              </p>
+              <p className="font-light mt-2">多元捐款方式</p>
             </li>
             <li className="step step-secondary">
               <p className="text-xl font-bold ">驗證</p>
-              <p className="font-light text-[0.5rem] mt-2">
-                我們將捐款轉換成USDT
-                <br />
-                並捐款給該提案
-              </p>
+              <p className="font-light mt-2">捐款驗證至區塊鏈</p>
             </li>
             <li className="step step-secondary">
               <p className="text-xl font-bold ">查看</p>
-              <p className="font-light text-[0.5rem] mt-2">
-                在區塊鏈上查看該捐款金流
-                <br />
-                或其他任何的金流
-              </p>
+              <p className="font-light mt-2">監督區塊鏈金流</p>
             </li>
           </ul>
         </div>
